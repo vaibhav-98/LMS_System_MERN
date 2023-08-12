ddf5c43fc96d799250a37701b30eaf9cf4b1bda8
vaibahv-98 <vaibhavtripathi1512@gmail.com>
2023-08-12 20:22:26 +0530

    push code

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,17 +1,24 @@
 import {Router} from "express"
-import { getProfile, login, logout, register } from "../controllers/user.controller.js"
+import { forgotPassword, getProfile, login, logout, register, resetPassword } from "../controllers/user.controller.js"
 import { isLoggedIn } from "../middelwares/auth.middelware.js"
+import upload from "../middelwares/multer.middelware.js"
 
 
 const router = Router()
 //****************************** Registion User*************************************/
-router.post('/register', register)
+router.post('/register',upload.single("avatar") ,register)
 //******************************login********************************************* */
 router.post('/login', login)
 //**************************logOut************************************************* */
 router.get('/logout', logout)
 //*********************************Get User**************************************** */
 router.get('/me', isLoggedIn, getProfile)
+
+//************************************** */
+router.post('/forgot/password' , forgotPassword)
+
+//
+router.post('/reset-password', resetPassword)
 
 
 
